916465ca6f7d93d17dbddfb4d5850856c7839a35
adomfosugit <adomfosugit@gmail.com>
2023-07-27 01:30:50 +0000

    change1

--- components/Product.jsx
+++ components/Product.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import { urlFor } from '../lib/client'
 import { useStateContext } from '../context/StateContext';
-import Image from 'next/image';
+
 const Product = ({product  }) => {
   const { qty, onAdd} = useStateContext();
+  
   return (
 <div className="bg-white shadow-lg rounded-lg overflow-hidden h-25">
      
@@ -15,9 +16,7 @@ const Product = ({product  }) => {
           className="w-full h-80 object-cover"
         />
         </Link> 
-        {/* <div className="absolute top-2 right-2 bg-white px-2 py-1 rounded-full">
-          <span className="text-gray-800 font-semibold"></span>
-        </div>*/}
+        
       </div>
       <div className="p-4">
         <h3 className="text-lg font-semibold text-gray-800 text-wrap font-gen tracking-[2px] ">{product.name}</h3>
@@ -26,7 +25,8 @@ const Product = ({product  }) => {
         </p>
        
         <div className="flex items-center justify-between mt-4 font-gen">
-          <span className="text-gray-800 font-bold">GHC {product.price}</span>
+          <span className="text-gray-800 font-bold">GHC {product.price}</span> 
+
           
         </div>
       </div>
@@ -38,30 +38,4 @@ const Product = ({product  }) => {
   )
 }
 
-export default Product  
-
-{/* <div className='flex flex-row items-center bg-zinc-200 mb-2 ml-3 rounded-xl shadow-xl sm:flex-col  phone: w-[340px] '>
-<Link href= {`/product/${product.slug.current}`}>
-          <div className= "product-card">
-              <img src={urlFor(product.image && product.image[0])} 
-              
-              className='product-image w-[200px] h-[200px] p-3'/>
-              
-          </div>
-          
-  </Link>
-              <div className='flex-col px-3'>
-               <div>
-              <Link   href= {`/product/${product.slug.current}`} >
-                  <p className='product-name'>{product.name}</p>
-              </Link>
-              <p className='product-price'>GHC  {product.price}</p>
-              </div>
-                  <div className='buttons flex'>
-                 
-                      <button type = 'button' 
-                      className='p-2 text-white bg-blue-700 rounded boder-none mt-4' onClick= {() => onAdd (product, qty)}>Add to cart </button>
-                     
-                  </div>
-                  </div>
-</div> */}
+export default Product 
