792099ba6e6a18e77c7e33ed8b7bbfc1ff6064c5
adomfosugit <adomfosugit@gmail.com>
2023-07-27 21:05:26 +0000

    change11

--- pages/Information.js
+++ pages/Information.js
@@ -2,23 +2,32 @@ import React from 'react'
 
 const Information = () => {
   return (
-    <div className='mx-3'>
-        <div>
+    <div className='mx-7 text-lg'>
+        <div >
             <h2 className='products-heading1 font-bold text-bold text-[30px] text-zinc-500'>About Nutorla</h2>
-            <p>Nutorla is a women's wear fashion brand established in 2017 to meet a quiet luxury fashion need or women between the ages of 23-50.
-          The brand focuses on using unique fabrics to achieve untraditional silhouettes that meet modern day fashion trends while satisfying a particular need of luxury and exclusivity for its women . The brand however is growing and unearthing different fashion needs with new ideas and projects that would soon extend the set age bracket and genders
+            <p>Nutorla is a women's wear fashion brand established in 2017 to meet a quiet luxury fashion need of women between the ages of 23-50.
+          The brand focuses on using unique fabrics to achieve untraditional silhouettes that meet modern day fashion trends while satisfying a particular standard of luxury and exclusivity for its women . The brand however is growing and unearthing different fashion needs with new ideas and projects that would soon extend the set age bracket and genders.
 
-         Established on the core grounds of luxury , attention to detail and an insatiable quest to set different fashion standards , Nutorla is the new style dispensation</p>
+         Established on the core grounds of luxury, attention to detail and an insatiable quest to set different fashion standards, Nutorla is the new style dispensation.</p>
         </div>
         <div>
             <h2 className='products-heading1 font-bold text-[30px] text-zinc-500'>Return Policy</h2>
             <p>
-            Clothing can be returned within 14-days after purchase for oversized or under sizing. 
+            Return policy 
+Thank you for your purchase, we hope you are satisfied with your items . If not, items can be returned within 7-14 working days upon receipt. The store reserves the right to either replace completely refund or offer store credits based on condition of the item. Items must be retuned in great shape if not original state 
+
+Returns and replacements do not apply to items purchased on sales.
+The store would not accept responsibility for items Client’s provided their own measurements for if they do not go with the standard measurement chart Nutorla Clothing has provided . 
+
+On replacement or adjustment of returned items, items must be sent directly to the store’s physical location, Humahu Street, Accra Ghana . The store would not be responsible for dispatch fees nor shipping back of items to customers. 
+
+Additional Information 
+Since the store has a physical location , items purchased are to be picked up and returned directly to the store. Dispatch services however would be provided to customers who cannot or may not be able to visit the store for pickups . 
             </p>
         </div>
         <div>
             <h2 className='products-heading1 font-bold text-[30px] text-zinc-500 justify-between'>Address</h2>
-            <p>Humahu St,
+            <p className='mx-auto'>Humahu St,
                 GA - 557- 1389,
                 Accra - Ghana </p>
         </div>
