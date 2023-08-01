9a91bc1e3b2cf1ac9c3ecff6deb28669d35dc529
adomfosugit <adomfosugit@gmail.com>
2023-08-01 13:28:41 +0000

    currencies test 37

--- components/Cartlogin.js
+++ components/Cartlogin.js
@@ -61,7 +61,7 @@ const handleClose = () => {
     reference: (new Date()).getTime(),
     username: `${user.name}`,
     email: `${user.email}`,
-    amount: `${100 * calculatePriceInGHCCurrency(totalPrice, selectedCurrency)}`,
+    amount: `${totalPrice * 100 * calculatePriceInGHCCurrency(totalPrice, selectedCurrency)}`,
     //publicKey: 'pk_live_7818dae2e6cdb971945ad64c97354ea97bc01afb',
     publicKey: 'pk_test_1156b935d863b0c6d92a19b3678d034562cf062a',
     currency: 'GHS',
