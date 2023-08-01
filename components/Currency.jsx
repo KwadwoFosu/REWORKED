1bddd128593a811c0fe465c43f89e28e9ee5a281
adomfosugit <adomfosugit@gmail.com>
2023-08-01 20:59:48 +0000

    currencies test 48

--- components/Currency.jsx
+++ components/Currency.jsx
@@ -15,7 +15,7 @@ const Currency = () => {
     <div className='text-black h-2'>
       <select onChange={handleSelectChange} value={selectedCurrency}>
         <option value="GHC">GHC</option>
-        <option value="USD">USD</option>
+        <option value="Yen">Yen</option>
         <option value="EUR">EUR</option>
         <option value="GBP">GBP</option>
       </select>
