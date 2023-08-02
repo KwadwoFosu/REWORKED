97d31370e2027e261e59b020cdd4542a2ea91bff
adomfosugit <adomfosugit@gmail.com>
2023-08-02 16:11:30 +0000

    currencies test

--- context/StateContext.js
+++ context/StateContext.js
@@ -88,7 +88,7 @@ export const StateContext = ({ children }) => {
   const onAdd = (product, quantity,size,color,selectedCurrency) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id);
     
-    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+    setTotalPrice((prevTotalPrice) => calculatePriceInCurrency(prevTotalPrice,selectedCurrency) + calculatePriceInCurrency(product.price,selectedCurrency) * quantity);
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
     
     if (checkProductInCart) {
@@ -117,8 +117,7 @@ export const StateContext = ({ children }) => {
     foundProduct = cartItems.find((item) => item._id === product._id);
     const newCartItems = cartItems.filter((item) => item._id !== product._id);
 
-    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
-
+    setTotalPrice((prevTotalPrice) => calculatePriceInCurrency(prevTotalPrice,selectedCurrency) - calculatePriceInCurrency(foundProduct.price,selectedCurrency) * foundProduct.quantity);
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity);
     setCartItems(newCartItems);
   };
