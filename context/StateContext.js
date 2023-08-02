e98faecaa3e7bbe2900ccefc972a5d8e2574e310
adomfosugit <adomfosugit@gmail.com>
2023-08-02 11:11:06 +0000

    currencies test 81

--- context/StateContext.js
+++ context/StateContext.js
@@ -22,7 +22,42 @@ export const StateContext = ({ children }) => {
     setTotalQuantities(0); // Reset the total quantities to zero
   };
   
-
+  const calculatePriceInCurrency = ( price,currency) =>{
+    // Perform any necessary calculations or conversions based on the currency here
+    // For simplicity, let's assume the conversion rates are already available
+    if (currency === EUR) {
+      return (price * 0.08).toFixed(2); // Assuming EUR is a predefined conversion rate
+    } else if (currency === GBP) {
+      return (price * 0.2).toFixed(2); // Assuming GBP is a predefined conversion rate
+    }
+    else if (currency === GHC) {
+      return (price * 1).toFixed(2); // Assuming GBP is a predefined conversion rate
+    }
+    else if (currency === USD) {
+      return (price * 0.088).toFixed(2); // Assuming GBP is a predefined conversion rate
+    }
+    else {
+      return price; // Return the original price if no matching currency is found
+    }
+  }
+  const calculatePriceInGHCCurrency = ( price,currency) =>{
+    // Perform any necessary calculations or conversions based on the currency here
+    // For simplicity, let's assume the conversion rates are already available
+    if (currency === EUR) {
+      return price/0.08; // Assuming EUR is a predefined conversion rate
+    } else if (currency === GBP) {
+      return price/0.2; // Assuming GBP is a predefined conversion rate
+    }
+    else if (currency === GHC) {
+      return price/1; // Assuming GBP is a predefined conversion rate
+    } 
+    else if (currency === USD) {
+      return price/0.088; // Assuming GBP is a predefined conversion rate
+    } 
+    else {
+      return price; // Return the original price if no matching currency is found
+    }
+  }
   let foundProduct;
   let index;
   useEffect(() => {
@@ -43,42 +78,7 @@ export const StateContext = ({ children }) => {
       setTotalQuantities(totalQuantities);
     }
   }, []);
-  const calculatePriceInCurrency = ( price,currency) =>{
-    // Perform any necessary calculations or conversions based on the currency here
-    // For simplicity, let's assume the conversion rates are already available
-    if (currency === 'EUR') {
-      return (price * 0.08).toFixed(2); // Assuming EUR is a predefined conversion rate
-    } else if (currency === 'GBP') {
-      return (price * 0.2).toFixed(2); // Assuming GBP is a predefined conversion rate
-    }
-    else if (currency === 'GHC') {
-      return (price * 1).toFixed(2); // Assuming GBP is a predefined conversion rate
-    }
-    else if (currency === 'USD') {
-      return (price * 0.088).toFixed(2); // Assuming GBP is a predefined conversion rate
-    }
-    else {
-      return price; // Return the original price if no matching currency is found
-    }
-  }
-  const calculatePriceInGHCCurrency = ( price,currency) =>{
-    // Perform any necessary calculations or conversions based on the currency here
-    // For simplicity, let's assume the conversion rates are already available
-    if (currency === 'EUR') {
-      return price/0.08; // Assuming EUR is a predefined conversion rate
-    } else if (currency === 'GBP') {
-      return price/0.2; // Assuming GBP is a predefined conversion rate
-    }
-    else if (currency === 'GHC') {
-      return price/1; // Assuming GBP is a predefined conversion rate
-    } 
-    else if (currency === 'USD') {
-      return price/0.088; // Assuming GBP is a predefined conversion rate
-    } 
-    else {
-      return price; // Return the original price if no matching currency is found
-    }
-  }
+  
     
 
   useEffect(() => {
