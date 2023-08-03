61b18d21cb005db1751315f8d377c7ae79a83ba5
adomfosugit <adomfosugit@gmail.com>
2023-08-03 18:35:20 +0000

    currencies test

--- components/Cartlogin.js
+++ components/Cartlogin.js
@@ -79,15 +79,15 @@ const handleClose = () => {
       // Perform any necessary calculations or conversions based on the currency here
       // For simplicity, let's assume the conversion rates are already available
       if (currency === 'EUR') {
-        return (price * Euro).toFixed(2); // Assuming EUR is a predefined conversion rate
+        return (price * 0.088).toFixed(2); // Assuming EUR is a predefined conversion rate
       } else if (currency === 'GBP') {
-        return (price * GBP).toFixed(2); // Assuming GBP is a predefined conversion rate
+        return (price * 0.8).toFixed(2); // Assuming GBP is a predefined conversion rate
       }
       else if (currency === 'GHC') {
         return (price * 1).toFixed(2); // Assuming GBP is a predefined conversion rate
       }
       else if (currency === 'USD') {
-        return (price * USD).toFixed(2); // Assuming GBP is a predefined conversion rate
+        return (price * 0.4).toFixed(2); // Assuming GBP is a predefined conversion rate
       }
       else {
         return price; // Return the original price if no matching currency is found
@@ -120,7 +120,7 @@ const handleClose = () => {
       ]
     }
 };
- 
+ {console.log(calculatePriceInGHCCurrency(totalPrice, 'EUR'))}
 // you can call this function anything
 const onSuccess = (reference) => {
   // Implementation for whatever you want to do with reference and after success call.
@@ -184,7 +184,7 @@ const PaystackHookExample = () => {
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
-                  <h4> GHC {calculatePriceInCurrency(item.price,selectedCurrency)}</h4>
+                  <h4> {selectedCurrency} {calculatePriceInCurrencyForeign(item.price,selectedCurrency)}</h4>
                 </div>
                 <div className="flex bottom">
                   <div>
