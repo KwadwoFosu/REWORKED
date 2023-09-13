c7f8696ca1fbde191d80a74ba162b01872c8ae0c
adomfosugit <adomfosugit@gmail.com>
2023-09-13 22:53:01 +0000

    paypal

--- components/Cartlogin.js
+++ components/Cartlogin.js
@@ -52,12 +52,6 @@ const handleClose = () => {
       //validity of form
       setFormValid(event.target.form.checkValidity());
     };
-    const handlePaymentSuccess = (paymentDetails) => {
-      // Payment was successful, you can now manually create an order
-      // using the paymentDetails and save it to your database
-      console.log('Payment succeeded:', paymentDetails);
-      // Add logic to save order data to your database manually
-    };
     //
     
     
@@ -281,12 +275,7 @@ const PaystackHookExample = () => {
             </div>
         <div className='btn-container'>
          <div > 
-         <button onClick={handleClick} className='btn' >checkout with Paystack</button>
-
-         //////////////
-         // PAY PAL ////
-         ///////////////
-         <PayPal amount={totalPrice} onSuccess={handlePaymentSuccess} cartItems={cartItems} />
+         <button onClick={handleClick} className='btn' >checkout </button>
          
          { showForm && (
           <form className="text-black fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-md rounded-lg p-6">
